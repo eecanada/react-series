40b5921ddb074472b7e8739a99c9d2f5dc9b3ae3
Eder Canada <canada.eder@gmail.com>
2020-02-10 10:09:12 -0800

    set state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,14 +10,20 @@ class App extends Component {
     series: []
   }
 
-  
+  componentDidMount(){
+    const series = ['Vikings', 'Game of thrones']
+    setTimeout(() => {
+      this.setState({ series: series})
+    },2000);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"> TV Series List </h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'> TV Series List </h1>
         </header>
-        <Intro message="Here you can find all of your most loved series"/>
+        <Intro message='Here you can find all of your most loved series'/>
         The length of series array - {this.state.series.length}
       </div>
     )

ef04adaa6914f28250b815e9d8e90ecae21b35dd
Eder Canada <canada.eder@gmail.com>
2020-02-10 15:34:06 -0800

    render SingleSeries component to display show

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
 
+import Loader from '../../components/Loader'
+
 
 class SingleSeries extends Component  {
+  state = {
+    show: null
+  }
+
+  componentDidMount(){
+    const {id} = this.props.match.params
+
+    fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+      .then(response => response.json())
+      .then(json => this.setState({ show: json }));
+  }
+
   render() {
+    const {show} = this.state
+    console.log(show)
     console.log(this.props)
     return (
-      <div> Single Series - the show id will be {this.props.match.params.id} </div>
+      <div>
+        { show === null && <Loader/>}
+        { show !== null && 
+        <div> 
+          <img style={{marginTop: 30}}src={show.image.medium} alt="show"/>
+          <p> {show.name} </p>
+          <p> Premiered {show.premiered} </p>
+          <p> Rating {show.average} </p>
+          <p> Episodes {show._embedded.episodes.length} </p>
+
+        </div>
+        }
+
+      </div>
     )
   }
 }
